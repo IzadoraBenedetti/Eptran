82b593687a9b8b9d191d4178814622ce01db938a
Guilherme Zermiani <134628581+Zermapaz@users.noreply.github.com>
2024-05-08 12:33:52 -0300

    Add files via upload

---
+++ Layout_EPTRAN_login2/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById('login-btn').addEventListener('click', function() {
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+
+  console.log(`Username: ${username}, Password: ${password}`);
+});
