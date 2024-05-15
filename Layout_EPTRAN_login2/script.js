48cf21ccacdd62d9aec8161fa1acaa1b33f810b4
Guilherme Zermiani <134628581+Zermapaz@users.noreply.github.com>
2024-05-15 11:12:14 -0300

    Delete Layout_EPTRAN_login2 directory

--- Layout_EPTRAN_login2/script.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('login-btn').addEventListener('click', function() {
-
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-
-
-  console.log(`Username: ${username}, Password: ${password}`);
-});
