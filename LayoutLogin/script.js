8b1fd807d86e3680460d2910672c7c64fedb9b03
Guilherme Zermiani <134628581+Zermapaz@users.noreply.github.com>
2024-05-15 11:46:08 -0300

    Delete LayoutLogin directory

--- LayoutLogin/script.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('login-btn').addEventListener('click', function() {
-
-  const username = document.getElementById('Usuario').value;
-  const password = document.getElementById('Senha').value;
-
-
-  console.log(`Usuario: ${username}, Senha ${password}`);
-});
